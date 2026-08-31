620e8e6684765c6c820c48303ec5c32916716763
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-31 16:09:22 +0200

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Suspense } from 'react';
@@ -87,6 +88,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <OfflineIndicator />
           <Toaster />
         </ThemeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
